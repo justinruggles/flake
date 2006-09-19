af90c82a7da98832a6ba8e4d55b0aaef28db7ebb
Justin Ruggles <justin.ruggles@gmail.com>
2006-09-19 13:46:01 +0000

    fix number parsing

--- flake/flake.c
+++ flake/flake.c
@@ -29,6 +29,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <string.h>
+#include <math.h>
 
 #if (defined __MINGW32__) || (defined _WIN32)
 #include <fcntl.h>
@@ -113,11 +114,16 @@ parse_number(char *arg, int max) {
     int i;
     int m = 1;
     int n = 0;
+    int digits;
     for(i=0; i<max; i++) {
         if(arg[i] == '\0') break;
+    }
+    digits = i;
+    m = pow(10, digits-1);
+    for(i=0; i<digits; i++) {
         if(arg[i] < '0' || arg[i] > '9') return -1;
         n += (arg[i]-48) * m;
-        m *= 10;
+        m /= 10;
     }
     return n;
 }
