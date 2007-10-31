f6178a70cc378f7ad6bba551d888c78323c39042
Justin Ruggles <justin.ruggles@gmail.com>
2007-10-31 01:02:00 +0000

    change to C-style comments

--- libflake/flake.h
+++ libflake/flake.h
@@ -58,116 +58,154 @@ typedef enum {
 
 typedef struct FlakeEncodeParams {
 
-    // compression quality
-    // Values 0 to 8 are libFLAC compatibility modes
-    // 0 is lower compression, faster encoding
-    // 8 is higher compression, slower encoding
-    // extended values 9 to 12 are specific to Flake
-    // 9 and 10 are within the FLAC Subset and use variable block size
-    // 11 and 12 use parameters such as higher block sizes and prediction
-    // orders which are outside of the FLAC Subset, so should not be used for
-    // streaming
+    /**
+     * compression quality
+     * Values 0 to 8 are libFLAC compatibility modes
+     * 0 is lower compression, faster encoding
+     * 8 is higher compression, slower encoding
+     * extended values 9 to 12 are specific to Flake
+     * 9 and 10 are within the FLAC Subset and use variable block size
+     * 11 and 12 use parameters such as higher block sizes and prediction
+     * orders which are outside of the FLAC Subset, so should not be used for
+     * streaming
+     */
     int compression;
 
-    // prediction order selection method
-    // if set to less than 0, it is chosen based on compression.
-    // valid values are 0 to 6
-    // 0 = use maximum order only
-    // 1 = use estimation
-    // 2 = 2-level
-    // 3 = 4-level
-    // 4 = 8-level
-    // 5 = full search
-    // 6 = log search
+    /**
+     * prediction order selection method
+     * if set to less than 0, it is chosen based on compression.
+     * valid values are 0 to 6
+     * 0 = use maximum order only
+     * 1 = use estimation
+     * 2 = 2-level
+     * 3 = 4-level
+     * 4 = 8-level
+     * 5 = full search
+     * 6 = log search
+     */
     int order_method;
 
-    // stereo decorrelation method
-    // if set to less than 0, it is chosen based on compression.
-    // valid values are 0 to 2
-    // 0 = independent L+R channels
-    // 1 = mid-side encoding
+    /**
+     * stereo decorrelation method
+     * if set to less than 0, it is chosen based on compression.
+     * valid values are 0 to 2
+     * 0 = independent L+R channels
+     * 1 = mid-side encoding
+     */
     int stereo_method;
 
-    // block size in samples
-    // if set to 0, a block size is chosen based on block_time_ms
+    /**
+     * block size in samples
+     * if set to 0, a block size is chosen based on block_time_ms
+     */
     int block_size;
 
-    // padding size in bytes
-    // if set to less than 0, defaults to 8192
+    /**
+     * padding size in bytes
+     * if set to less than 0, defaults to 8192
+     */
     int padding_size;
 
-    // minimum prediction order
-    // if set to less than 0, it is chosen based on compression.
-    // valid values are 0 to 4 for fixed prediction and 1 to 32 for non-fixed
+    /**
+     * minimum prediction order
+     * if set to less than 0, it is chosen based on compression.
+     * valid values are 0 to 4 for fixed prediction and 1 to 32 for non-fixed
+     */
     int min_prediction_order;
 
-    // maximum prediction order
-    // if set to less than 0, it is chosen based on compression.
-    // valid values are 0 to 4 for fixed prediction and 1 to 32 for non-fixed
+    /**
+     * maximum prediction order
+     * if set to less than 0, it is chosen based on compression.
+     * valid values are 0 to 4 for fixed prediction and 1 to 32 for non-fixed
+     */
     int max_prediction_order;
 
-    // type of linear prediction
-    // if set to less than 0, it is chosen based on compression.
-    // 0 = fixed prediction
-    // 1 = Levinson-Durbin recursion
+    /**
+     * type of linear prediction
+     * if set to less than 0, it is chosen based on compression.
+     * 0 = fixed prediction
+     * 1 = Levinson-Durbin recursion
+     */
     int prediction_type;
 
-    // minimum partition order
-    // if set to less than 0, it is chosen based on compression.
-    // valid values are 0 to 8
+    /**
+     * minimum partition order
+     * if set to less than 0, it is chosen based on compression.
+     * valid values are 0 to 8
+     */
     int min_partition_order;
 
-    // maximum partition order
-    // if set to less than 0, it is chosen based on compression.
-    // valid values are 0 to 8
+    /**
+     * maximum partition order
+     * if set to less than 0, it is chosen based on compression.
+     * valid values are 0 to 8
+     */
     int max_partition_order;
 
-    // whether to use variable block sizes
-    // if set to 1, libflake will automatically split each frame into smaller
-    // frames in order to improve compression
-    // 0 = fixed block size
-    // 1 = variable block size
+    /**
+     * whether to use variable block sizes
+     * if set to 1, libflake will automatically split each frame into smaller
+     * frames in order to improve compression
+     * 0 = fixed block size
+     * 1 = variable block size
+     */
     int variable_block_size;
 
-    // whether to let the user send frames with varying block size to the
-    // encoder this will allow a user to experiment with their own variable
-    // block size algorithms if they choose to do so.
+    /**
+     * whether to let the user send frames with varying block size to the
+     * encoder this will allow a user to experiment with their own variable
+     * block size algorithms if they choose to do so.
+     */
     int allow_vbs;
 
 } FlakeEncodeParams;
 
 typedef struct FlakeContext {
 
-    // number of audio channels
-    // set by user prior to calling flake_encode_init
-    // valid values are 1 to 8
+    /**
+     * number of audio channels
+     * set by user prior to calling flake_encode_init
+     * valid values are 1 to 8
+     */
     int channels;
 
-    // audio sample rate in Hz
-    // set by user prior to calling flake_encode_init
+    /**
+     * audio sample rate in Hz
+     * set by user prior to calling flake_encode_init
+     */
     int sample_rate;
 
-    // sample size in bits
-    // set by user prior to calling flake_encode_init
-    // only 16-bit is currently supported
+    /**
+     * sample size in bits
+     * set by user prior to calling flake_encode_init
+     * only 16-bit is currently supported
+     */
     int bits_per_sample;
 
-    // total stream samples
-    // set by user prior to calling flake_encode_init
-    // if 0, stream length is unknown
+    /**
+     * total stream samples
+     * set by user prior to calling flake_encode_init
+     * if 0, stream length is unknown
+     */
     unsigned int samples;
 
-    // encoding parameters
-    // all fields must be set by user (or by flake_set_defaults) prior to
-    // calling flake_encode_init
+    /**
+     * encoding parameters
+     * all fields must be set by user (or by flake_set_defaults) prior to
+     * calling flake_encode_init
+     */
     FlakeEncodeParams params;
 
-    // header bytes
-    // allocated by flake_encode_init and freed by flake_encode_close
+    /**
+     * header bytes
+     * allocated by flake_encode_init and freed by flake_encode_close
+     */
     unsigned char *header;
 
-    // encoding context, which is hidden from the user
-    // allocated by flake_encode_init and freed by flake_encode_close
+    /**
+     * encoding context, which is hidden from the user
+     * allocated by flake_encode_init and freed by flake_encode_close
+     */
     void *private_ctx;
 
 } FlakeContext;
